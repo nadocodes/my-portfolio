f6817fc9521e1211560e3ba98bbdeaca8a46aecd
nadocodes <naderantar96@gmail.com>
2023-02-06 15:29:28 +0000

    setup App.jsx appropriately, setup components and constants

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import styles from './style'
+
+import { Navbar, Hero, About, Projects, Contact, Button, Skills } from './components';
 
 const App = () => (
-    <div className='bg-primary w-full overflow-hidden'>
-      <h1>Hello, World!</h1>
+  <div className="bg-primary w-full overflow-hidden">
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Navbar />
+        </div>
+      </div>
+    
+    <div classname={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Hero />
+      </div>
     </div>
+
+    <div classname={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <About />
+        <Projects />
+        <Skills />
+        <Contact />
+      </div>
+    </div>
+
+  </div>
 );
 
 
---
+++ src/components/index.js
@@ -0,0 +1,17 @@
+import Navbar from './Navbar';
+import Hero from './Hero';
+import About from './About';
+import Projects from './Projects';
+import Contact from './Contact';
+import Button from './Button';
+import Skills from './Skills';
+
+export {
+    Navbar,
+    Hero,
+    About,
+    Projects,
+    Contact,
+    Button,
+    Skills,
+};
